70ed39b37004dbd235353ffd1c425a9dad8db9bb
Darin Critchlow <darincritchlow@gmail.com>
2015-03-27 15:12:17 -0600

    refactor list with arguments

--- wsh.cpp
+++ wsh.cpp
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <ftw.h>
 #include <dejagnu.h>
 //#include <InstallerPlugins/InstallerPlugins.h>
 //#include <InstallerPlugins/InstallerPlugins.h>
@@ -25,6 +26,8 @@ char*	fmodes[] = { "---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx" };
 // -----------------------------------------------------------------------------------
 
 
+
+static int display_info(const char *fpath, const struct stat *sb, int tflag);
 
 wsh::wsh()
 {
@@ -94,7 +97,6 @@ void wsh::interpret()		// loops executing commands from the command line.
 
 			case LIST :
 				list();
-				cout << endl;
 				break;
 			case VIEW :
 				view();
@@ -179,7 +181,7 @@ bool wsh::copy(char* src, char* dest)
 }
 
 void wsh::cd(){
-
+	cd(argv[1]);
 }
 
 void wsh::cd(char* dir) {
@@ -198,14 +200,22 @@ void wsh::list() {
 
 int wsh::list(int argc, char **argv) {
 	struct stat statbuf;
+	struct FTW ftwbuf;
 //	struct stat statbuf2;
 //	struct tm *t;
-//	int rc;
+	int flags = 0;
 	char buffer[PATH_MAX];
 	DIR *dir;
 	struct dirent *ent;
+	char *parent = cwd;
 
-	if (argc != 2) {
+
+	if(strcmp(argv[0], "list") == 0 && argc ==1){
+		puts("not -l");
+
+//			char *currentDir = strcat(cwd, "/");
+//			currentDir = strcat(currentDir, argv[1]);
+
 		if ((dir = opendir (cwd)) != NULL) {
 
 			while( (ent = readdir(dir)) != NULL ) {
@@ -213,17 +223,16 @@ int wsh::list(int argc, char **argv) {
 					continue;
 
 				lstat(ent->d_name, &statbuf);
-
 				printf("%s", ent->d_name);
-
-				if( S_ISDIR(statbuf.st_mode) )
+				if (ent->d_type & DT_DIR)
 					puts("/");
 				else if ( statbuf.st_mode & S_IXUSR )
 					puts("*");
 				else
 					puts("");
+
 			}
-			closedir (dir);
+			closedir(dir);
 		}
 		else {
 			/* could not open directory */
@@ -232,67 +241,201 @@ int wsh::list(int argc, char **argv) {
 		}
 	}
 
-	if (argc == 2) {
-		if (strcmp(argv[1], "-l") != 0) {
-			cout << argv[1] << ":" << endl;
+	if (strcmp(argv[0], "list") == 0 && strcmp(argv[1], "-l") == 1 && argc == 2) {
+		puts("not -l");
 
-			char *currentDir = strcat(cwd, "/");
-			currentDir = strcat(currentDir, argv[1]);
+		char *currentDir = strcat(cwd, "/");
+		currentDir = strcat(currentDir, argv[1]);
 
-			if ((dir = opendir (currentDir)) != NULL) {
+		if ((dir = opendir (currentDir)) != NULL) {
 
-				while( (ent = readdir(dir)) != NULL ) {
-					if( strcmp(ent->d_name, ".") == 0 ||  strcmp(ent->d_name, "..") == 0)
-						continue;
+			while( (ent = readdir(dir)) != NULL ) {
+				if( strcmp(ent->d_name, ".") == 0 ||  strcmp(ent->d_name, "..") == 0)
+					continue;
 
-					lstat(ent->d_name, &statbuf);
+				lstat(ent->d_name, &statbuf);
 
-					printf("%s", ent->d_name);
+				if (ent->d_type & DT_DIR)
+					puts("/");
+				else if ( statbuf.st_mode & S_IXUSR )
+					puts("*");
+				else
+					puts("");
 
-					if (ent->d_type & DT_DIR)
-						puts("/");
-					else if ( statbuf.st_mode & S_IXUSR )
-						puts("*");
-					else
-						puts("");
-
-				}
-				closedir (dir);
 			}
-			else {
-				/* could not open directory */
-				perror (cwd);
-				return EXIT_FAILURE;
-			}
+			closedir(dir);
 		}
-
-		if (strcmp(argv[1], "-l") == 0) {
-			puts("yes its  -l");
-			if ((dir = opendir(cwd)) != NULL) {
-				while ((ent = readdir(dir))) {
-					if (ent->d_type & DT_REG)
-						cout << ent->d_name << endl;
-					else if (ent->d_type & DT_DIR) if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, ".."))
-						cout << ent->d_name << endl;
-					else
-						cout << ent->d_name << "/" << endl;
-					else if (ent->d_type & DT_LNK)
-						cout << ent->d_name << " is a symbolic link" << endl;
-					else if (strcmp(ent->d_name, ".") || strcmp(ent->d_name, ".."))
-						cout << ent->d_name << endl;
-					else
-						cout << ent->d_name << " is something other than a file or directory" << endl;
-				}
-				closedir(dir);
-			} else {
-				/* could not open directory */
-				perror(argv[1]);
-				return EXIT_FAILURE;
-			}
+		else {
+			/* could not open directory */
+			perror (cwd);
+			return EXIT_FAILURE;
 		}
+		return EXIT_SUCCESS;
 	}
-	return EXIT_SUCCESS;
+
+//	if (strcmp(argv[0], "list") == 0 && argc == 1) {
+//		if ((dir = opendir (cwd)) != NULL) {
+//
+//			while( (ent = readdir(dir)) != NULL ) {
+//				if( strcmp(ent->d_name, ".") == 0 ||  strcmp(ent->d_name, "..") == 0)
+//					continue;
+//
+//				lstat(ent->d_name, &statbuf);
+//
+//				printf("%s", ent->d_name);
+//
+//				if( S_ISDIR(statbuf.st_mode) )
+//					puts("/");
+//				else if ( statbuf.st_mode & S_IXUSR )
+//					puts("*");
+//				else
+//					puts("");
+//			}
+//			closedir (dir);
+//		}
+//		else {
+//			/* could not open directory */
+//			perror (cwd);
+//			return EXIT_FAILURE;
+//		}
+////		flags |= FTW_DEPTH;
+////		if (nftw(cwd, display_info, 20, flags) == -1)
+////		{
+////			perror("nftw");
+////			return EXIT_FAILURE;
+////		}
+////		ftw(".", display_info, -1);
+//		return EXIT_SUCCESS;
+//	}
+
+
+
+//	if (argc == 2){
+//		if (strcmp(argv[1], "-l") == 0 && argc == 2) {
+//			puts("2nd arg is -l");
+//			if ((dir = opendir(cwd)) != NULL) {
+//
+//				while ((ent = readdir(dir)) != NULL) {
+//					if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+//						continue;
+//
+//					lstat(ent->d_name, &statbuf);
+//
+//					printf((S_ISDIR(statbuf.st_mode)) ? "d" : "-");
+//					printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
+//					printf((statbuf.st_mode & S_IWUSR) ? "w" : "-");
+//					printf((statbuf.st_mode & S_IXUSR) ? "x" : "-");
+//					printf((statbuf.st_mode & S_IRGRP) ? "r" : "-");
+//					printf((statbuf.st_mode & S_IWGRP) ? "w" : "-");
+//					printf((statbuf.st_mode & S_IXGRP) ? "x" : "-");
+//					printf((statbuf.st_mode & S_IROTH) ? "r" : "-");
+//					printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
+//					printf((statbuf.st_mode & S_IXOTH) ? "x" : "-");
+//					printf("\t%s\n", ent->d_name);
+//
+//
+//				}
+//				closedir(dir);
+//			}
+//			else {
+//				/* could not open directory */
+//				perror(cwd);
+//				return EXIT_FAILURE;
+//			}
+//		}
+
+
+
+//		if (strcmp(argv[1], "-l")) {
+//			puts("2nd arg is dir name");
+//			char *currentDir = strcat(cwd, "/");
+//			currentDir = strcat(currentDir, argv[1]);
+//			puts(currentDir);
+//
+//			if ((dir = opendir (currentDir)) != NULL) {
+//
+//				while( (ent = readdir(dir)) != NULL ) {
+//					if( strcmp(ent->d_name, ".") == 0 ||  strcmp(ent->d_name, "..") == 0)
+//						continue;
+//
+//					lstat(ent->d_name, &statbuf);
+//					printf("%s", ent->d_name);
+//					if (ent->d_type & DT_DIR)
+//						puts("/");
+//					else if ( statbuf.st_mode & S_IXUSR )
+//						puts("*");
+//					else
+//						puts("");
+//
+//				}
+//				closedir(dir);
+//			}
+//			else {
+//				/* could not open directory */
+//				perror (cwd);
+//				return EXIT_FAILURE;
+//			}
+//		}
+//	}
+//	if (argc > 2) {
+//		char *currentDir = strcat(cwd, "/");
+//		currentDir = strcat(currentDir, argv[1]);
+//
+//		if ((dir = opendir(currentDir)) != NULL) {
+//
+//			while ((ent = readdir(dir)) != NULL) {
+//				if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+//					continue;
+//
+//				lstat(ent->d_name, &statbuf);
+//
+//				printf((S_ISDIR(statbuf.st_mode)) ? "d" : "-");
+//				printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
+//				printf((statbuf.st_mode & S_IWUSR) ? "w" : "-");
+//				printf((statbuf.st_mode & S_IXUSR) ? "x" : "-");
+//				printf((statbuf.st_mode & S_IRGRP) ? "r" : "-");
+//				printf((statbuf.st_mode & S_IWGRP) ? "w" : "-");
+//				printf((statbuf.st_mode & S_IXGRP) ? "x" : "-");
+//				printf((statbuf.st_mode & S_IROTH) ? "r" : "-");
+//				printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
+//				printf((statbuf.st_mode & S_IXOTH) ? "x" : "-");
+//				printf("\t%s", ent->d_name);
+//
+//
+//			}
+//			closedir(dir);
+//		}
+//		else {
+//			/* could not open directory */
+//			perror(cwd);
+//			return EXIT_FAILURE;
+//		}
+//	}
+//	return EXIT_SUCCESS;
+
+
 }
+
+int display_info(const char *fpath, const struct stat *sb, int type)
+{
+	printf("%s", fpath);
+	if( S_ISDIR(sb->st_mode) )
+		puts("/");
+	else if ( sb->st_mode & S_IXUSR )
+		puts("*");
+	else
+		puts("");
+	return 0;
+}
+
+//static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
+//{
+//	printf("%7jd %s%s\n",
+//
+//			(intmax_t) sb->st_size,
+//			fpath + ftwbuf->base, (tflag == FTW_D) ? "/" : "");
+//	return 0;           /* To tell nftw() to continue */
+//}
 
 
 void wsh::view() {
